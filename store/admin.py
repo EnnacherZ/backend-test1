829a5bc2b96d8ee01e92ae7d2d710a3f75b8031f
EnnacherZ <zakariaeennacher@gmail.com>
2024-12-26 15:23:48 +0100

    GJBK

--- store/admin.py
+++ store/admin.py
@@ -12,15 +12,4 @@ admin.site.register(Pant)
 admin.site.register(PantDetail)
 admin.site.register(Client)
 admin.site.register(ProductOrdered)
-class ShoeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'image_thumbnail')  # Affiche le nom, prix et une miniature de l'image
-    search_fields = ('name',)  # Recherche par nom
-    list_filter = ('price',)  # Filtrage par prix
-
-    def image_thumbnail(self, obj):
-        if obj.image:
-            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')  # Génère une miniature
-        return None
-    image_thumbnail.short_description = 'Image'
-
-admin.site.register(Shoe, ShoeAdmin)
+admin.site.register(Shoe)
